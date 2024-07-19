fed3027cbf60f33992817efac4bfd7a67394fe0f
Kamilla Aisakhunova <kamillaaisakhunova@MacBook-Air-Kamilla.local>
2024-07-19 19:56:59 +0500

    fix errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import { BrowserRouter as Router, Route, Routes, Link, useLocation } from "react-router-dom";
 import MainPage from "./pages/MainPage";
 import DonePage from "./pages/DonePage";
